093a4b567769f51a03eff0c3283aa6840d8f6241
WazDevZm <wazingwamugala90@gmail.com>
2026-06-22 23:30:41 +0200

    feat : learnt the concat() function

--- JS/Hello.js
+++ JS/Hello.js
@@ -7,3 +7,19 @@ console.log(PI);
 // this is my third line of code in JS, trying to get a JOB in MANGOS 🥭
 
 console.log ("Printing out my name , Wazingwa Lol");
+
+
+// ways and methods of performing String Concatenation
+// this is method 1 of concatenation
+let greeting = "Hello ";
+greeting += ", Wazingwa!";
+console.log(greeting);
+
+
+//. using the concat() method to concatenate
+let firstName = "Wazingwa";
+let surName = "Mugala";
+
+let fullName = firstName.concat(" " + surName);
+
+console.log(fullName);
