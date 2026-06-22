20788a7f85662e74d95a4bd8ce61730e998c9abf
WazDevZm <wazingwamugala90@gmail.com>
2026-06-22 23:06:15 +0200

    feat: add folder structure of the learning path.

---
+++ JS/Hello.js
@@ -0,0 +1,7 @@
+let name = "Wazingwa";
+// this is my first line of code in JS😭
+console.log("Hello, " + name + "!");
+// this is my secod line of code in JS
+const PI = 3.14;
+console.log(PI);
+// this is my third line of code in JS, trying to get a JOB in MANGOS 🥭
