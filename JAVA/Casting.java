bafd6eba9533b6b7b8b58686dd22de59d7d49e73
WazDevZm <wazingwamugala90@gmail.com>
2026-07-10 13:22:51 +0200

    add type-casting

---
+++ JAVA/Casting.java
@@ -0,0 +1,12 @@
+
+/* this file will hold all the code i need to learn how to type-cast in java */
+
+public class Casting {
+    public static void main(String[] args) {
+        byte b = 42;
+        short s = b;
+        int i = s;
+
+    }
+    
+}
---
+++ JAVA/PassByValue2.java
@@ -0,0 +1,22 @@
+class Student{
+    String name;
+    int age;
+
+    public Student(String name, int age){
+        this.name = name;
+        this.age = age;    
+    }
+    //this is a toString Method
+    public String toString(){
+        return "Student name " + name + ",student age " + age;
+    }
+
+}
+// this code tries to Pass Objects as Arguments in Java Language
+public class PassByValue2{
+    public static void main(String[] args) {
+  
+
+      
+    }
+}
